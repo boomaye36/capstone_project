1a8ea3c86d3fa6bd9bc7ac1b11b795e0186989fb
송현근 <timxxz@naver.com>
2023-03-15 19:16:14 +0900

    환경설정

---
+++ src/main/java/com/capstone/test/TestController.java
@@ -0,0 +1,28 @@
+package com.capstone.test;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.capstone.test.dao.TestDAO;
+
+@Controller
+ class TestController {
+	@Autowired
+	private TestDAO testdao;
+	@RequestMapping(" /test")
+	public String test(Model model) {
+		return "test";
+	}
+	@ResponseBody
+
+	@RequestMapping("/db")
+	public List<Map<String, Object>> databaseTest() {
+		return testdao.selectTestList();
+	}
+}
---
+++ src/main/java/com/capstone/test/User.java
@@ -0,0 +1,5 @@
+package com.capstone.test;
+
+public class User {
+
+}
